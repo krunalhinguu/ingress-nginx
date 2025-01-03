d7b54c1d56c8abdb459b2e4fa816b99684035b1a
Krunal Hingu <krunal.hingu222@gmail.com>
2025-01-03 12:59:33 +0530

    temp: release specific tag

--- scripts/check-for-new-tag.sh
+++ scripts/check-for-new-tag.sh
@@ -70,7 +70,8 @@ else
     done
 fi
 
-echo "NEW_TAGS=${new_tags[@]}" >> $GITHUB_ENV
+# echo "NEW_TAGS=${new_tags[@]}" >> $GITHUB_ENV
+echo "NEW_TAGS=1.11.4" >> $GITHUB_ENV
 
 # Clean up temporary files
 rm -f "$rancher_tags_file" "$upstream_tags_file"
