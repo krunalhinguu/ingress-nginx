275db8ddd1306b1f735a7cab29ef7050b621dc97
Krunal Hingu <krunal.hingu222@gmail.com>
2025-01-04 05:11:28 +0530

    update the tag list

--- scripts/check-for-new-tag.sh
+++ scripts/check-for-new-tag.sh
@@ -71,7 +71,7 @@ else
 fi
 
 # echo "NEW_TAGS=${new_tags[@]}" >> $GITHUB_ENV
-echo "NEW_TAGS=1.11.4" >> $GITHUB_ENV
+echo "NEW_TAGS=1.12.0" >> $GITHUB_ENV
 
 # Clean up temporary files
 rm -f "$rancher_tags_file" "$upstream_tags_file"
